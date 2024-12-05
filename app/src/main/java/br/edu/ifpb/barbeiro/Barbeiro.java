f82bee8b725b9553415039ea162ded9d41415427
Pedroo722 <pedrohenriquealexandre7@gmail.com>
2024-12-05 10:39:54 -0300

    Commit Inicial

---
+++ app/src/main/java/br/edu/ifpb/barbeiro/Barbeiro.java
@@ -0,0 +1,25 @@
+package br.edu.ifpb.barbeiro;
+
+public class Barbeiro extends Thread {
+	private Barbearia barbearia;
+	private boolean cortar = true;
+
+	public Barbeiro(Barbearia barbearia) {
+		this.barbearia = barbearia;
+	}
+
+	public void run() {
+		while (cortar) {
+			try {
+				int sleepTime;
+				do {
+					sleepTime = ((int) (Math.random() * 5000));
+				} while (sleepTime < 1000);
+				Thread.sleep(sleepTime);
+				barbearia.cortarCabelo();
+			} catch (Exception e) {
+			}
+		}
+	}
+
+}
---
+++ app/src/main/java/br/edu/ifpb/barbeiro/Cliente.java
@@ -0,0 +1,31 @@
+package br.edu.ifpb.barbeiro;
+
+public class Cliente extends Thread {
+	private Barbearia barbearia;
+	private boolean cortar = true;
+	private String nomeCliente;
+
+	public Cliente(Barbearia barbearia, String nome) {
+		this.barbearia = barbearia;
+		this.nomeCliente = nome;
+	}
+
+	public void run() {
+		while (cortar) {
+			try {
+				int sleepTime;
+				do {
+					sleepTime = ((int) (Math.random() * 5000));
+				} while (sleepTime < 1000);
+
+				Thread.sleep(sleepTime);
+				barbearia.aguardaVez(this);
+			} catch (Exception e) {
+			}
+		}
+
+	}
+	public String nomeCliente(){
+		return nomeCliente;
+	}
+}
