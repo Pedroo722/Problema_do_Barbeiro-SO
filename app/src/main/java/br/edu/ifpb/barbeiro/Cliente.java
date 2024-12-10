dd09e8d7e86b69c9be8d64dd7fabd7da73c35130
Pedroo722 <pedrohenriquealexandre7@gmail.com>
2024-12-10 14:11:51 -0300

    feature: Implementação de BarbeiroDorminhoco com synchronized Co-authored-by: ViniciusCavalcantePequeno <vinicius.pequeno@academico.ifpb.edu.br>

--- app/src/main/java/br/edu/ifpb/barbeiro/Cliente.java
+++ app/src/main/java/br/edu/ifpb/barbeiro/Cliente.java
@@ -1,14 +1,61 @@
 package br.edu.ifpb.barbeiro;
 
-public class Cliente implements Runnable {
-    private final Barbeiro barbeiro;
+/**
+ * A classe Cliente representa um cliente que chega à barbearia para cortar o cabelo.
+ * Cada cliente é uma thread que simula a ação de ir à barbearia, onde ele é adicionado à fila de espera
+ * para ser atendido por um barbeiro.
+ * 
+ * O cliente possui um identificador único e, ao executar a thread, ele é adicionado à fila da barbearia
+ * para aguardar o atendimento do barbeiro.
+ * 
+ * @author ViniciusCavalcantePequeno
+ */
+public class Cliente extends Thread {
+    private Barbearia barbearia;
+    private long clienteId;
 
-    public Cliente(Barbeiro barbeiro) {
-        this.barbeiro = barbeiro;
+    /**
+     * Construtor da classe Cliente.
+     * Inicializa a barbearia onde o cliente deseja cortar o cabelo.
+     * 
+     * @param barbearia instância da barbearia onde o cliente vai cortar o cabelo
+     */
+    public Cliente(Barbearia barbearia) {
+        this.barbearia = barbearia;
     }
 
+    /**
+     * Método responsável por retornar o identificador único do cliente.
+     * 
+     * @return identificador único do cliente
+     */
+    public long getClienteId() {
+        return clienteId;
+    }
+
+    /**
+     * Método responsável por definir o identificador único do cliente.
+     * 
+     * @param clienteId identificador único a ser atribuído ao cliente
+     */
+    public void setClienteId(long clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    /**
+     * Método que simula a chegada do cliente à barbearia.
+     * O cliente é adicionado à fila da barbearia para aguardar o atendimento.
+     */
+    private void irCortarCabelo() {
+        barbearia.adicionarCliente(this);
+    }
+
+    /**
+     * Método que executa a thread do cliente. Quando o cliente é iniciado,
+     * ele chama o método `irCortarCabelo()` para se adicionar à fila da barbearia.
+     */
     @Override
     public void run() {
-        barbeiro.chegarCliente();
+        irCortarCabelo();
     }
 }
---
+++ app/src/main/java/br/edu/ifpb/barbeiro/GeradorCliente.java
@@ -0,0 +1,45 @@
+package br.edu.ifpb.barbeiro;
+
+/**
+ * A classe GeradorCliente é responsável por simular a chegada de novos clientes à barbearia.
+ * Ela estende a classe Thread e, ao ser executada, cria e inicia novas instâncias da classe Cliente.
+ * 
+ * O comportamento da classe consiste em gerar clientes de forma contínua, com intervalos de tempo
+ * aleatórios de até 4 segundos, para simular a chegada de clientes de maneira imprevisível.
+ * 
+ * @author ViniciusCavalcantePequeno
+ */
+public class GeradorCliente extends Thread {
+    private Barbearia barbearia;
+
+    /**
+     * Construtor da classe GeradorCliente.
+     * Inicializa a instância de GeradorCliente com a referência para a barbearia onde os clientes serão registrados.
+     * 
+     * @param barbearia a instância da barbearia onde os clientes serão atendidos
+     */
+    public GeradorCliente(Barbearia barbearia) {
+        this.barbearia = barbearia;
+    }
+
+    /**
+     * Método executado pela thread para gerar clientes de forma contínua.
+     * A cada ciclo, um novo cliente é criado, iniciado como uma thread e adicionado à fila de espera da barbearia.
+     * Entre a criação de cada cliente, a thread pausa por um tempo aleatório de até 4 segundos.
+     */
+    @Override
+    public void run() {
+        while (true) {
+            Cliente cliente = new Cliente(barbearia); // cria cliente
+            cliente.start(); // inicia como thread
+            cliente.setClienteId(cliente.getId());
+
+            try {
+                // Pausa a execução por um tempo aleatório entre 0 e 4 segundos
+                Thread.sleep((long) (Math.random() * 4000));
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+        }
+    }
+}
